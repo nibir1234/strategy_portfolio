e001bee52a537d4a0198941d8938be7ea2a31bf0
Thomas Martin <thomasmartin1783@gmail.com>
2023-04-19 04:17:29 -0600

    svg scrolltrigger line

--- app.js
+++ app.js
@@ -304,3 +304,18 @@ window.onload = function () {
   css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid blue}";
   document.body.appendChild(css);
 };
+
+
+
+
+
+let path = document.querySelector('path')
+let pathLength = path.getTotalLength()
+path.style.strokeDasharray = pathLength + ' ' + pathLength;
+path.style.strokeDashoffset = pathLength
+
+window.addEventListener('scroll', () => {
+  var scrollPercentage = (document.documentElement.scrollTop + document.body.scrollTop) / (document.documentElement.scrollHeight - document.documentElement.clientHeight)
+  var drawLength = pathLength * (scrollPercentage)
+  path.style.strokeDashoffset = pathLength - drawLength
+})
